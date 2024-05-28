9026f76fea404d22612080c58c645ed3d2a3723d
rok <rok.rak1@gmail.com>
2024-05-28 18:27:45 +0200

    termiante workers after completion

--- src/services/cron.service.ts
+++ src/services/cron.service.ts
@@ -28,16 +28,47 @@ export const addCronJob = async (
             `Puppeteer task for botId ${bot.id} completed successfully:`,
             result.result
           );
+          worker.terminate().then(async () => {
+            await serverLogger(
+              LoggerLevels.ERROR,
+              `Worker for botId ${bot.id} terminated successfully.`
+            );
+            console.log(`Worker for botId ${bot.id} terminated successfully.`);
+          });
         } else {
           console.error(
             `Error in Puppeteer task for botId ${bot.id}:`,
             result.error
           );
+          worker.terminate().then(async () => {
+            await serverLogger(
+              LoggerLevels.ERROR,
+              `Worker for botId ${bot.id} terminated after encountering an error.`
+            );
+            console.log(
+              `Worker for botId ${bot.id} terminated after encountering an error.`
+            );
+          });
         }
       });
 
-      worker.on("error", (error) => {
+      worker.on("error", async (error) => {
         console.error("Worker error:", error);
+        worker.terminate().then(async () => {
+          await serverLogger(
+            LoggerLevels.ERROR,
+            `Worker for botId ${bot.id} terminated due to an internal error.`
+          );
+          console.log(
+            `Worker for botId ${bot.id} terminated due to an internal error.`
+          );
+        });
+      });
+
+      worker.on("exit", (code) => {
+        if (code !== 0) {
+          console.error(`Worker stopped with exit code ${code}`);
+        }
       });
 
       worker.on("exit", (code) => {
